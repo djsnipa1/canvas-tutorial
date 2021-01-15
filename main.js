dbb8657f8ba7b855580b582139fee40f5bddb60e
djsnipa1 <chadboyce@gmail.com>
2021-01-15 05:54:12 -0500

    made it into shell shape

--- main.js
+++ main.js
@@ -3,29 +3,35 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+ctx.globalCompositeOperation = 'destination-over';
 
-let positionX = canvas.width / 4;
-let positionY = canvas.height / 4;
-let angle = 0;
+let number = 0;
+let scale = 10;
+let hue = Math.random() * 360;
 
 function drawFlower() {
-  ctx.fillStyle = 'red';
-  ctx.strokeStyle = 'orange';
+  let angle = number * .05;
+  let radius = scale * Math.sqrt(number);
+  let positionX = radius * Math.sin(angle) + canvas.width / 2;
+  let positionY = radius * Math.cos(angle) + canvas.height / 2;
+
+  ctx.fillStyle = 'hsl(' + hue + ', 100%, 50%)';
+  ctx.strokeStyle = 'black';
   ctx.lineWidth = 3;
   ctx.beginPath();
-  ctx.arc(positionX, positionY, 20, 0, Math.PI * 2);
+  ctx.arc(positionX, positionY, number, 0, Math.PI * 2);
   ctx.closePath();
   ctx.fill();
   ctx.stroke();
+
+  number++;
+  hue += 1.5;
 }
 
 function animate() {
   // draw each frame
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  
-  positionX += 5 * Math.sin(angle);
-  positionY += 5 * Math.cos(angle);
-  angle += 0.1;
+  //ctx.clearRect(0, 0, canvas.width, canvas.height);
+  if (number > 300) return;
   drawFlower();
   requestAnimationFrame(animate);
 }
