687aa30e1e44f406215fcb418de61ca6a6446bec
djsnipa1 <chadboyce@gmail.com>
2021-02-04 15:03:34 -0500

    worked on tutorial 4

--- tutorial4/script.js
+++ tutorial4/script.js
@@ -15,3 +15,47 @@ window.addEventListener('mousemove', function(event){
     mouse.y = event.y;
     console.log(mouse.x);
 })
+
+class Root {
+  constructor(x, y, color, centerX, centerY){
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    this.speedX = speedX;
+    this.speedY = speedY;
+    this.centerX = centerX;
+    this.centerY = centerY;
+  }
+  draw(){
+    this.speedX += (Math.random() - 0.5 / 2);
+    this.speedY += (Math.random() - 0.5 / 2);
+    this.x += this.speedX;
+    this.y += this.speedY;
+    
+    const distanceX = this.x - this.centerX;
+    const distanceY = this.y - this.centerY;
+    const distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
+    const radius = (-distance / edge + 1) * edge / 10;
+    
+    if (radius > 0) {
+      requestAnimationFrame(this.draw);
+      ctx.beginPath;
+      ctx.arc(this.x, this.y, radius, 0, 2 * Math.PI);
+      ctx.fillStyle = this.color;
+      ctx.fill();
+      ctx.strokeStyle = 'gold';
+      ctx.stroke();
+    }
+    
+  }
+}
+
+function branchOut() {
+  const centerX = mouse.x;
+  const centerY = mouse.y;
+  for (let i = 0; i < 3; i++){
+    const root = new Root(mouse.x, mouse.y, );
+    // x, y, color, centerX, centerY
+    
+  }
+}
