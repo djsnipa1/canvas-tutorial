7c03f4cedcdbff33aad9a756f4dac5fa94a22d66
Chad Boyce <chadboyce@gmail.com>
2021-02-13 13:40:23 +0000

    I got gitpod auto preview working

--- tutorial5/script.js
+++ tutorial5/script.js
@@ -1,6 +1,8 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 console.log(canvas);
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 window.addEventListener('resize', function () {
     canvas.width = window.innerWidth;
@@ -10,8 +12,9 @@ window.addEventListener('resize', function () {
 
 ctx.fillStyle = 'red';
 ctx.strokeStyle = 'yellow';
-ctx.lineWidth - 5;
+ctx.lineWidth = 5;
 ctx.beginPath();
 ctx.arc(100, 100, 50, 0, Math.PI * 2);
 ctx.stroke();
+ctx.fill();
 console.log(ctx);
